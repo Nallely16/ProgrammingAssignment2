5efa56d9e7463d95c8008fbf290578b4bd4a38cf
karthi-7-a <61533714+karthi-7-a@users.noreply.github.com>
2021-01-28 18:48:07 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,32 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(A = matrix()) {
+  Ai <- NULL    ## set the var that will cache the inverse matrix as NULL 
+  set <- function(y) {   ## Set the value of the matrix 
+    A <<- y
+    Ai <<- NULL          ## Clear the value of the inverse matrix if the original matrix change
+  }
+  get <- function() A ## Show the elements of the matrix 
+  setInverse <- function(inverse) Ai <<- inverse ## Stores the inverse matrix in the var 
+  getInverse <- function() Ai # Shows the inverse matrix, if already calculated  
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Receive a special matrix created by makeCacheMatrix as input and calculates its inverse if
+## the matrix is invertible  
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(A, ...) {
+  Ai <- A$getInverse() # Get the var stored in the special matrix 
+  if (!is.null(Ai)) {   #  checks if the inverse matrix has already been calculated and stored
+    message("getting cached data") #If TRUE, just return the inverse matrix. 
+    return(Ai)
+  }
+  data <- A$get()      # retrieves the elements from the original matrix
+  Ai <- solve(data)    #calculates the inverse matrix (if possible)
+  A$setInverse(Ai)     # stores the inverse matrix so that it can be consulted later
+  Ai
 }
